52057375d2a65a05e3b013737a78a7d2310b8662
Dasha <lomakodasha@gmail.com>
2019-11-09 22:24:59 +0300

    init: add basic layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { VacancyComponent } from './pages/vacancy/vacancy.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'vacancy', component: VacancyComponent },
+  { path: 'contacts', component: ContactsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { VacancyComponent } from './pages/vacancy/vacancy.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ProductsComponent,
+    VacancyComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
